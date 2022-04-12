6665480fe3aa423c09f6b47ed93f0cbea1937aaa
kotnid <tkt0506tkt@gmail.com>
2022-04-12 19:23:07 +0800

    osu_parser : Add func to get data in pairs
    Signed-off-by: kotnid <tkt0506tkt@gmail.com>

--- osu_parser.py
+++ osu_parser.py
@@ -28,17 +28,17 @@ def get_line(phrase):
     for num, line in enumerate(osu_data, 0):
         if phrase in line:
             return num
-    if phrase == '[Colours]':
-        get_line('[HitObjects]')
+    if phrase == '[colours]':
+        return get_line('[hitObjects]') -1
 
-general_line = get_line('[General]')
-editor_line = get_line('[Editor]')
-metadata_line = get_line('[Metadata]')
-difficulty_line = get_line('[Difficulty]')
-events_line = get_line('[Events]')
-timing_line = get_line('[TimingPoints]')
-Colours_line = get_line('[Colours]')
-hit_line = get_line('[HitObjects]')
+general_line = get_line('[general]')
+editor_line = get_line('[editor]')
+metadata_line = get_line('[metadata]')
+difficulty_line = get_line('[difficulty]')
+events_line = get_line('[events]')
+timing_line = get_line('[timingPoints]')
+Colours_line = get_line('[colours]')
+hit_line = get_line('[hitObjects]')
 
 
 # Get datas of each section
@@ -48,5 +48,16 @@ metadata_list = osu_data[metadata_line:difficulty_line-1]
 difficulty_list = osu_data[difficulty_line:events_line-1]
 events_list = osu_data[events_line:timing_line-1]
 timingpoints_list = osu_data[timing_line:Colours_line-1]
-Colours_list = osu_data[Colours_line:hit_line-1]
+colours_list = osu_data[Colours_line:hit_line-1]
 hitobject_list = osu_data[hit_line:]
+
+
+# Transfer datas of section which content type is pairs
+pairs_sections = [general_list , editor_list , metadata_list , difficulty_list , colours_list]
+pairs_name = ['general' , 'editor' , 'metadata' , 'difficulty' , 'colours' ]
+
+for i in range(len(pairs_sections)):
+    for item in pairs_sections[i]:
+        if ':' in item:
+            item = item.split(': ')
+            data[pairs_name[i]][item[0]] = item[1]
