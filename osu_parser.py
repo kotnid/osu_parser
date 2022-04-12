ce9c0a9871846cf5e7711a48b91c1222f5a19301
kotnid <tkt0506tkt@gmail.com>
2022-04-12 19:00:42 +0800

    osu_parser : Add func to get line of datas
    Signed-off-by: kotnid <tkt0506tkt@gmail.com>

--- osu_parser.py
+++ osu_parser.py
@@ -7,7 +7,7 @@ root.withdraw()
 osu_file = filedialog.askopenfilename()
 osu_data = open(osu_file,'r+').readlines()
 
-# Init place for data to store
+# Init place for datas to store
 data = {}
 data['General'] = {}
 data['Editor'] = {}
@@ -17,3 +17,18 @@ data['Events'] = []
 data['timingpoints'] = []
 data['Colours'] = {}
 data['hitobjects'] = []
+
+# Get line of datas
+def get_line(phrase):
+    for num, line in enumerate(osu_data, 0):
+        if phrase in line:
+            return num
+
+general_line = get_line('[General]')
+editor_line = get_line('[Editor]')
+metadata_line = get_line('[Metadata]')
+difficulty_line = get_line('[Difficulty]')
+events_line = get_line('[Events]')
+timing_line = get_line('[TimingPoints]')
+Colours_line = get_line('[Colours]')
+hit_line = get_line('[HitObjects]')
