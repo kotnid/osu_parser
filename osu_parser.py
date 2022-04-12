cf6e8b22e36bb32bbdb59eea280bd2ed6b109ae3
kotnid <tkt0506tkt@gmail.com>
2022-04-12 18:58:11 +0800

    osu_parser : Init place for data to store
    Signed-off-by: kotnid <tkt0506tkt@gmail.com>

--- osu_parser.py
+++ osu_parser.py
@@ -1,7 +1,19 @@
 from tkinter import filedialog
 import tkinter as tk
 
+# Get file to parse
 root = tk.Tk()
 root.withdraw()
 osu_file = filedialog.askopenfilename()
 osu_data = open(osu_file,'r+').readlines()
+
+# Init place for data to store
+data = {}
+data['General'] = {}
+data['Editor'] = {}
+data['Metadata'] = {}
+data['Difficulty'] = {}
+data['Events'] = []
+data['timingpoints'] = []
+data['Colours'] = {}
+data['hitobjects'] = []
